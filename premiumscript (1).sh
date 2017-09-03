af9ed69af9dc79a7416a86d78ae199f43021ad00
samreysteven <samreysteven91@gmail.com>
2017-09-04 07:53:52 +0800

    Add files via upload

---
+++ premiumscript (1).sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#script copyright www.autoscriptnobita.tk
+echo
+echo
+echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+echo "Hubungi: Archangels (Email: admin@autoscriptnobita.tk atau https://telegram.me/archangels )"
+
